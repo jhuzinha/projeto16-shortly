f4d08b338274e258feebaa76bcd8f24f12d6e2d7
Jhuliana <jholeanasantos@gmail.com>
2022-08-05 14:57:51 -0300

    fix: register auth

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -13,7 +13,7 @@ export async function signUp(req, res){
     const { name, email, password, confirmPassword } = req.body;
     const passwordHash = bcrypt.hashSync(password, 10);
     try {
-        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3, $4)`, [name, email, passwordHash])
+        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, passwordHash])
         res.status(200).send("User created");
     } catch (error) {
         console.log(error)

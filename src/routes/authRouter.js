60e975ccaeade91c2a22ddd547fea439996440da
Jhuliana <jholeanasantos@gmail.com>
2022-08-05 17:41:25 -0300

    fix: validator register

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -5,6 +5,6 @@ import Validate from "../middlewares/joiValidationMiddleware.js";
 const authRouter = Router();
 
 authRouter.post("/login", )
-authRouter.post("/register", Validate(register), signUp)
+authRouter.post("/register", Validate('register'), signUp)
 
 export default authRouter;
